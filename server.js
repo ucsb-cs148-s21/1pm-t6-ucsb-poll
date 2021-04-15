20ae6fe7784fbb2dca362dfee545b77ed864e5e4
dahamtob <dshamtob@yahoo.com>
2021-04-15 14:59:52 -0700

    fixed another path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/build/index.js'))
   });
 
 
