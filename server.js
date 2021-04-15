8a41752809400217a9aed4aa49952b4932eb23ff
dahamtob <dshamtob@yahoo.com>
2021-04-15 14:52:33 -0700

    added backend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  });
+
+
+const PORT = process.env.PORT || 8080;
+  
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
