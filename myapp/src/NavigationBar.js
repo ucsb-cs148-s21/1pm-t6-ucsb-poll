ccffebf10fd4c1ef63e8f86ef8a812d517a39c69
Jasun Chen <chen.jasun@gmail.com>
2021-04-16 01:47:05 -0700

    updated navbar names

--- myapp/src/NavigationBar.js
+++ myapp/src/NavigationBar.js
@@ -14,7 +14,7 @@ render() {
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#profile">Profile</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
