b491041f1fba54250cf8eac9402e6a43b8610e04
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-16 16:30:23 -0700

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -27,6 +27,8 @@ class App extends Component {
 
         <div class="jumbotron">
           <h1 class="display-4">Hello World, this is UCSB polls</h1> 
+        </div>
+      </div>
       <div class="card-deck">
         <div class="card">
           <img class="card-img-top" src="poll result graph" alt="Card image cap"/>
