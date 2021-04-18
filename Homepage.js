ee68f555e1c0c811410a07c95dc71f169cee3b61
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-18 02:20:06 -0700

    Delete Homepage.js

--- Homepage.js
+++
@@ -1,9 +0,0 @@
-
-//not a working one right now, just for my team to view
-const db=firebase.database().ref();
-const pools=db.child('pools');
-const sortbydate=pools.oderByChild('date').limitToFirst(6);
-const sortbypop=pools.oderByChild('participants').limitToFirst(6);
-
-sortbydate.once('value',snap => console.log(snap.val()))
-
