3d89e6e277adacd8b4b2e8e4e06b8726591aa061
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-23 14:48:45 -0700

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,17 +4,7 @@ import DemoPoll from "./DemoPoll";
 
 
 
-<script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
-const db=firebase.database().ref();
-<div id='messagesDiv'></div>
-db.on('value', function(snapshot) {
-	var message = snapshot.val();
-	displayMessage(message.answerable, message.date, message.question);
-});
-function displayMessage(answerable, daye, question) {
-    $('#messagesDiv').append('<div>answerable:'+answerable+',date:'+date+',question:'+question+'</div>');
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  };
+
 class App extends Component {
   constructor(props) {
     super(props);
