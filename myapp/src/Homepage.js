5b12e84b57170d655028fd629c0eba2f0f29f611
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-23 15:17:27 -0700

    Update Homepage.js

--- myapp/src/Homepage.js
+++ myapp/src/Homepage.js
@@ -8,7 +8,7 @@ const sortbypop=pools.oderByChild('participants').limitToFirst(6);
 
 sortbydate.once('value',snap => console.log(snap.val()))
 */
-    
+/*
 <script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
 var db = new Firebase('https://ucsb-polls.firebaseIO.com/');
 <div id='messagesDiv'></div>
@@ -20,3 +20,36 @@ function displayMessage(answerable, daye, question) {
     $('#messagesDiv').append('<div>answerable:'+answerable+',date:'+date+',question:'+question+'</div>');
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
   };
+*/
+/*
+<script src="/__/firebase/5.5.7/firebase-app.js"></script>
+<script src="/__/firebase/5.5.7/firebase-auth.js"></script>
+<script src="/__/firebase/5.5.7/firebase-database.js"></script>
+
+https://console.firebase.google.com/project/ucsb-polls/firestore/data~2Fpolls?hl=zh-cn
+
+<script src="/__/firebase/init.js"></script>
+*/
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCmZ272B89syKA0FNLa7ujYHvfI60YB2M0",
+    authDomain: "ucsb-polls.firebaseapp.com",
+    projectId: "ucsb-polls",
+    storageBucket: "ucsb-polls.appspot.com",
+    messagingSenderId: "989606767140",
+    appId: "1:989606767140:web:cf485612653f0ba2a186b1",
+    measurementId: "G-0HG55T6LG9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
