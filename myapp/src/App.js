c02940d5508eec9bafe2b861c1404dfa1daf0050
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-23 15:31:23 -0700

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,36 @@ import NavigationBar from "./components/NavigationBar.js";
 import DemoPoll from "./DemoPoll";
 
 
+var firebaseConfig = {
+	apiKey: "AIzaSyCmZ272B89syKA0FNLa7ujYHvfI60YB2M0",
+   	authDomain: "ucsb-polls.firebaseapp.com",
+    	projectId: "ucsb-polls",
+    	storageBucket: "ucsb-polls.appspot.com",
+    	messagingSenderId: "989606767140",
+    	appId: "1:989606767140:web:cf485612653f0ba2a186b1",
+    	measurementId: "G-0HG55T6LG9"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database=firebase.database();
+database.ref('/').once('value', function(snapshot){
+  console.log(snapshot.val());
+});
 
+//var rootRef=database().ref();
+var rootRef = database.ref('/');
+rootRef.once('value', function(snapshot){
+  console.log(snapshot.val());
+});
+poolDataRef = database.ref("/pools");
+//const pools=db.child('pools');
+database.ref('/pools').once('value', function(snapshot){
+  snapshot.forEach(function(data){
+    console.log("Below are the child keys of the values in 'pools'")
+    console.log(data.key);
+  });
+  console.log(Object.keys(snapshot.val()));
+});
 
 class App extends Component {
   constructor(props) {
