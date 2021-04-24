53dd73ea13dd0982c965fdb920219b7443b8c8b6
闪电球 <lzy@shandianqiudeMacBook-Air.local>
2021-04-24 16:07:57 -0700

    A

--- myapp/src/Homepage.js
+++ myapp/src/Homepage.js
@@ -8,82 +8,14 @@ const sortbypop=pools.oderByChild('participants').limitToFirst(6);
 
 sortbydate.once('value',snap => console.log(snap.val()))
 */
-/*
-<script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
-var db = new Firebase('https://ucsb-polls.firebaseIO.com/');
-<div id='messagesDiv'></div>
-db.on('value', function(snapshot) {
-	var message = snapshot.val();
-	displayMessage(message.answerable, message.date, message.question);
-});
-function displayMessage(answerable, daye, question) {
-    $('#messagesDiv').append('<div>answerable:'+answerable+',date:'+date+',question:'+question+'</div>');
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  };
-*/
-/*
-<script src="/__/firebase/5.5.7/firebase-app.js"></script>
-<script src="/__/firebase/5.5.7/firebase-auth.js"></script>
-<script src="/__/firebase/5.5.7/firebase-database.js"></script>
 
-https://console.firebase.google.com/project/ucsb-polls/firestore/data~2Fpolls?hl=zh-cn
-
-<script src="/__/firebase/init.js"></script>
-
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  */
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
-
-var firebaseConfig = {
-	apiKey: "AIzaSyCmZ272B89syKA0FNLa7ujYHvfI60YB2M0",
-   	authDomain: "ucsb-polls.firebaseapp.com",
-    	projectId: "ucsb-polls",
-    	storageBucket: "ucsb-polls.appspot.com",
-    	messagingSenderId: "989606767140",
-    	appId: "1:989606767140:web:cf485612653f0ba2a186b1",
-    	measurementId: "G-0HG55T6LG9"
-};
-
-firebase.initializeApp(firebaseConfig);
-var database=firebase.database();
-database.ref('/').once('value', function(snapshot){
-  console.log(snapshot.val());
-});
-
-var rootRef = database.ref('/');
-rootRef.once('value', function(snapshot){
-  console.log(snapshot.val());
-});
-
-function polls(){
-  var data = document.getElementById("dataValue").value;
-  var dataRef = database.ref('/polls');
-  dataRef.set({
-    value: data
-  });
+import { response } from "express";
+import useSWR from "swr";
+function getppoll(){
+  const fetcher = url => fetch(url).then(response=>response.json())
+  const{data}=useSWR(
+    `/api/getpoll/${date}`,
+    fetcher
+  );
+  const 
 }
-var pollsDataRef = database.ref("/polls");
-pollsDataRef.on("child_added", function(snapshot){
-  console.log("Below is the data from child_added");
-  console.log(snapshot.val());
-});
-
-database.ref('/polls').once('value', function(snapshot){
-  snapshot.forEach(function(data){
-    console.log("Below are the child keys of the values in 'polls'")
-    console.log(data.key);
-  });
-  console.log(Object.keys(snapshot.val()));
-});
