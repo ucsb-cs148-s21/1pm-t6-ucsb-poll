e3e3181d55de6d4178cb66f06559f11623aa7299
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-27 13:16:30 -0700

    Update hom.js

--- myapp/src/hom.js
+++ myapp/src/hom.js
@@ -47,7 +47,7 @@ class Poll {
   }
 }
 // Firestore data converter
-var pollConverter = {
+const pollConverter = {
   toFirestore: function(poll) {
       return {
           answerable: poll.answerable,
@@ -60,90 +60,47 @@ var pollConverter = {
       return new Poll(data.answerable, data.date, data.question);
   }
 };
-class HelloMessage extends React.Component {
-    render() {
-      var list=[];
-      /*
-      asyncFunction(list);
-      console.log(list)
-      console.log(list.first())
-      
-        console.log('I was triggered during render')
-        let list=[]
-        db.collection("users").get().then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc)  {
-              console.log(`${doc.id} => ${doc.data().name}`);
-              list.push(`${doc.data().name}`)
-              
-              console.log(list[0])
-              
-          });
-         
-      });
-      console.log(list)
-      */
-     /*
-     let p1=[]; 
-      var citiesRef = db.collection("users");
-      citiesRef.where("role", "==", "admin").get().then(function(p){
-        let k=p._delegate._snapshot.docChanges;
-        console.log(k);
-        console.log(k[0]);
-        console.log(k[0].doc.data.partialValue.mapValue.fields)
-        
-        p1[0]=(k[0].doc.data.partialValue.mapValue.fields.name.stringValue)
-        p1[1]=(k[1].doc.data.partialValue.mapValue.fields.name.stringValue)
-        p1[2]=(k[2].doc.data.partialValue.mapValue.fields.name.stringValue)
-        console.log(p1)
-      });
-      
-      //.then((res) =>{
-     //   console.log(res);
-   // });
 
-      console.log(p1[0])
-      */
-     /*
-     db.collection("users").where("role", "==", "admin")
-     .onSnapshot((querySnapshot) => {
-         var cities = [];
-         querySnapshot.forEach((doc) => {
-             cities.push(doc.data().name);
-             list.push(doc.data().name)
-         });
-         console.log("here", cities.join(", "));
-     });
- 
+const initialList = [];
 
-     console.log('I was triggered during render')
-     db.collection("users").get().then((querySnapshot) => {
-       querySnapshot.docs.map(function(doc){
-           console.log(`${doc.id} => ${doc.data().name}`);
-           list.push(`${doc.data().name}`)
-       });
-   });
-   */
-  var poppp=[];
-       
+function HelloMessage(){
+  const [list, setList] = React.useState(initialList);
   db.collection("polls").orderBy("date").limit(6).onSnapshot("value", function(snapshot) {
-      var questions = [];
       snapshot.forEach(function(data) {
-          questions.push(data.data().question);
-          poppp.push(data.data().question);
+        const newList = list.concat(JSON.stringify(data.data()))
+        setList(newList);
       });
-      poppp.push(questions);
+
   });
 
-    console.log(poppp)
-    console.log(poppp.length)
-    console.log(poppp[0])
-    console.log(poppp[2][0])
-      return ( 
-        <div>
-          {list[0]}
-        </div>
-      );
-      
-   }
+  return (
+    <div>{list}</div>
+  );
 }
+// class HelloMessage extends React.Component {
+//     render() {
+//       var list=[];
+//   var poppp=[];
+       
+//   db.collection("polls").orderBy("date").limit(6).onSnapshot("value", function(snapshot) {
+//       var questions = [];
+//       snapshot.forEach(function(data) {
+//           questions.push(data.data().question);
+//           poppp.push(data.data().question);
+//       });
+//       poppp.push(questions);
+//   });
+//     console.log("here")
+//     console.log(poppp)
+//     console.log(poppp.length)
+//     console.log(poppp[0])
+//     // console.log(poppp[2][0])
+//       return ( 
+//         <div>
+//           {list[0]}
+//         </div>
+//       );
+      
+//    }
+// }
 export default HelloMessage;
