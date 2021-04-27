0b75c33f85e50f9d228421d1b4ff77a73f00e5e3
zhengyingl <35554906+zhengyingl@users.noreply.github.com>
2021-04-27 04:16:19 -0700

    Update hom.js

--- myapp/src/hom.js
+++ myapp/src/hom.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import { ThemeProvider } from 'react-bootstrap';
 
 
 
@@ -26,19 +27,123 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 var db = firebase.firestore();
-
+/*
+function asyncFunction(list){
+  setTimeout(function(){
+    db.collection("users").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc)  {
+          console.log(`${doc.id} => ${doc.data().name}`);
+          list.push(`${doc.data().name}`)  
+      });
+  });
+  });
+}
+*/
+class Poll {
+  constructor (answerable, date, question ) {
+      this.answerable = answerable;
+      this.date = date;
+      this.question = question;
+  }
+}
+// Firestore data converter
+var pollConverter = {
+  toFirestore: function(poll) {
+      return {
+          answerable: poll.answerable,
+          date: poll.date,
+          question: poll.question
+          };
+  },
+  fromFirestore: function(snapshot, options){
+      const data = snapshot.data(options);
+      return new Poll(data.answerable, data.date, data.question);
+  }
+};
 class HelloMessage extends React.Component {
-    
     render() {
-        var citiesRef = db.collection("users");
-        var query = citiesRef.where("role", "==", "admin");
+      var list=[];
+      /*
+      asyncFunction(list);
+      console.log(list)
+      console.log(list.first())
+      
         console.log('I was triggered during render')
-        console.log(query)
+        let list=[]
+        db.collection("users").get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc)  {
+              console.log(`${doc.id} => ${doc.data().name}`);
+              list.push(`${doc.data().name}`)
+              
+              console.log(list[0])
+              
+          });
+         
+      });
+      console.log(list)
+      */
+     /*
+     let p1=[]; 
+      var citiesRef = db.collection("users");
+      citiesRef.where("role", "==", "admin").get().then(function(p){
+        let k=p._delegate._snapshot.docChanges;
+        console.log(k);
+        console.log(k[0]);
+        console.log(k[0].doc.data.partialValue.mapValue.fields)
+        
+        p1[0]=(k[0].doc.data.partialValue.mapValue.fields.name.stringValue)
+        p1[1]=(k[1].doc.data.partialValue.mapValue.fields.name.stringValue)
+        p1[2]=(k[2].doc.data.partialValue.mapValue.fields.name.stringValue)
+        console.log(p1)
+      });
+      
+      //.then((res) =>{
+     //   console.log(res);
+   // });
+
+      console.log(p1[0])
+      */
+     /*
+     db.collection("users").where("role", "==", "admin")
+     .onSnapshot((querySnapshot) => {
+         var cities = [];
+         querySnapshot.forEach((doc) => {
+             cities.push(doc.data().name);
+             list.push(doc.data().name)
+         });
+         console.log("here", cities.join(", "));
+     });
+ 
+
+     console.log('I was triggered during render')
+     db.collection("users").get().then((querySnapshot) => {
+       querySnapshot.docs.map(function(doc){
+           console.log(`${doc.id} => ${doc.data().name}`);
+           list.push(`${doc.data().name}`)
+       });
+   });
+   */
+  var poppp=[];
+       
+  db.collection("polls").orderBy("date").limit(6).onSnapshot("value", function(snapshot) {
+      var questions = [];
+      snapshot.forEach(function(data) {
+          questions.push(data.data().question);
+          poppp.push(data.data().question);
+      });
+      poppp.push(questions);
+  });
+
+    console.log(poppp)
+    console.log(poppp.length)
+    console.log(poppp[0])
+    console.log(poppp[2][0])
       return ( 
         <div>
-          Hello {this.props.name}
+          {list[0]}
         </div>
       );
-    }
+      
+   }
 }
 export default HelloMessage;
