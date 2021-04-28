bd0f892a498daaeb1e16cb2ede9ba9c955cce72b
Randp <yutongluo@umail.ucsb.edu>
2021-04-28 10:31:22 +0800

    bug

--- myapp/src/components/NewPolls/NewVote.js
+++ myapp/src/components/NewPolls/NewVote.js
@@ -2,6 +2,36 @@ import React, { useState } from 'react';
 
 import VoteForm from './VoteForm';
 import './NewVote.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { ThemeProvider } from 'react-bootstrap';
+
+
+
+
+<body>
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js">
+</script>
+<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-firestore.js">
+</script>
+</body>
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCmZ272B89syKA0FNLa7ujYHvfI60YB2M0",
+    authDomain: "ucsb-polls.firebaseapp.com",
+    projectId: "ucsb-polls",
+    storageBucket: "ucsb-polls.appspot.com",
+    messagingSenderId: "989606767140",
+    appId: "1:989606767140:web:cf485612653f0ba2a186b1",
+    measurementId: "G-0HG55T6LG9"
+};
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+var db = firebase.firestore();
+
 
 const NewVote = (props) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -11,7 +41,15 @@ const NewVote = (props) => {
       ...enteredVoteData,
       id: Math.random().toString(),
     };
-    props.onAddVote(VoteData);
+    db.collection("usertest").add(VoteData)
+  .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+
+   // props.onAddVote(VoteData);
     setIsEditing(false);
   };
 
