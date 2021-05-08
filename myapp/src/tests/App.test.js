dfb6e9210d1bddd51e3be1a2372ca02798262cba
Jasun Chen <chen.jasun@gmail.com>
2021-05-07 20:50:41 -0700

    updated form component testing to include user clicks

--- myapp/src/tests/App.test.js
+++ myapp/src/tests/App.test.js
@@ -4,6 +4,7 @@ import React from 'react'
 // import { setupServer } from 'msw/node'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 
 
 // example code for testing taken from documentation 
@@ -40,10 +41,29 @@ test('renders navigation bar when page loads', () => {
 });
 
 
-test('renders form to input new polls when page loads', () => {
+test('renders form to input new polls when page loads', async () => {
     render(<App />);
     expect(screen.queryByText(/Create a New Poll/i)).toBeInTheDocument(); 
     expect(screen.queryByText(/Add Poll Information/i)).toBeInTheDocument();
+
+    userEvent.click(screen.getByText('Add Poll Information'))
+    // fireEvent.click(screen.getByText('Add Poll Information')); can also be used
+
+    /*
+        waitFor is meant to be used with an assertion. In other words it is meant to wait until an expectation is true
+        Once the statement is true, we know the process is finished and we can continue to test 
+        We know that if we click the button, something like "question" should show up, so we wait for that. 
+    */
+    await waitFor(() =>
+        expect(screen.getByText(/Question/)).toBeInTheDocument()
+    )
+
+    // for some reason, querybylabeltext doesn't work, so I just use query by text
+
+    expect(screen.queryByText(/Question/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Category/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Due Date/i)).toBeInTheDocument(); 
+    expect(screen.queryByText(/Vote option1/i)).toBeInTheDocument(); 
 });
 
 
