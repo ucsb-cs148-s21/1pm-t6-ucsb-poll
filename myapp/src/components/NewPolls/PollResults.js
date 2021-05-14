21760a09863895e3b182f20eef9341b7a89f2b8f
dahamtob <dshamtob@yahoo.com>
2021-05-14 01:57:09 -0700

    reduced poll results size

--- myapp/src/components/NewPolls/PollResults.js
+++ myapp/src/components/NewPolls/PollResults.js
@@ -164,12 +164,12 @@ class PollResults extends Component {
                             
                         ) : (
                                 <div className="result">
-                                    <div style={{marginBottom: 50}}>
+                                    <div style={{marginBottom: 20}}>
                                         <span className="result" > {member.name}</span>
                                         <div id="blockContainer">
-                                            <div class={bars[index]} style={{width: member.voteCount > 0 ? this.calculatePercent(member.voteCount, totalVotes) : "0.1%", float: "left"}}></div>
-                                            <div style={{marginTop: 10, marginRight: 40, marginLeft: 10, float: "initial"}}>{member.chosen && <Checkmark size="medium" />}</div>
-                                            <div style={{marginTop: 10, marginRight: 10, position: "absolute", right: 0}}>{this.calculatePercent(member.voteCount, totalVotes)}</div>
+                                            <div className={"ResultBar " + bars[index]} style={{width: member.voteCount > 0 ? this.calculatePercent(member.voteCount, totalVotes) : "0.1%", float: "left"}}></div>
+                                            <div style={{marginTop: 4, marginRight: 40, marginLeft: 10, float: "initial"}}>{member.chosen && <Checkmark size="medium" />}</div>
+                                            <div style={{marginTop: 4, marginRight: 10, position: "absolute", right: 0}}>{this.calculatePercent(member.voteCount, totalVotes)}</div>
                                         </div>
                                     </div >
                                     
