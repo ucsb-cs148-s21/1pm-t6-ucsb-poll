aa08688cabda1807c3f90c621077f715faebbf85
闪电球 <lzy@shandianqiudeMacBook-Air.local>
2021-05-16 18:47:12 -0700

    try for update user's history

--- myapp/src/Main.js
+++ myapp/src/Main.js
@@ -3,13 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
 import ProfilePage from './components/ProfilePage';
+import PollPage from './components/PollPage';
+
 
 const Main = () => {
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/profile' component={ProfilePage}></Route>
-      {/* <Route path="/poll/:pollID" component={PollPage} /> */} 
+      <Route exact path='/profile' component={ProfilePage}></Route>  
+      <Route exact path="/poll/:pollid" component={PollPage} /> 
     </Switch>
   );
 }
